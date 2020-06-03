bf907efe261e83c17a91ee7a9439927ead368516
TheVIperShow <thevipershowita@gmail.com>
2020-06-03 14:09:33 +0200

    Added project structure

---
+++ src/main/java/me/thevipershow/koranplugin/KoranPlugin.java
@@ -0,0 +1,11 @@
+package me.thevipershow.koranplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KoranPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
